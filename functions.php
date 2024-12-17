6f9f8c62d1a68d37de2c2d3aba68dd8e12aedd52
SLARK <au949971@gmail.com>
2024-12-17 21:21:54 +0700

    Как я заебался

---
+++ functions.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Форматирует цену лота - разделяет пробелом разряды числа, добавляет знак рубля
+ * @param integer $num Цена лота
+ * @return string Как цена будет показываться в карточке
+*/
+function format_num ($num) {
+    $num = ceil($num);
+    $num = number_format($num, 0, '', ' ');
+
+    return "$num ₽";
+    }
+
+//function console_log( $data ){
+//    echo '<script>';
+//    echo 'console.log('. json_encode( $data ) .')';
+//    echo '</script>';
+//    }
+
+/**
+ * Возвращеет количество целых часов и остатка минут от настоящего времени до даты
+ * @param string $date Дата истечения времени
+ * @return array
+*/
+function get_time_left ($date) {
+    date_default_timezone_set('Europe/Moscow');
+    $final_date = date_create($date);
+    $cur_date = date_create("now");
+    $diff = date_diff($final_date, $cur_date);
+    $format_diff = date_interval_format($diff, "%d %H %I");
+    $arr = explode(" ", $format_diff);
+
+    $hours = $arr[0] * 24 + $arr[1];
+    $minutes = intval($arr[2]);
+    $hours = str_pad($hours, 2, "0", STR_PAD_LEFT);
+    $minutes = str_pad($minutes, 2, "0", STR_PAD_LEFT);
+
+    $res[] = $hours;
+    $res[] = $minutes;
+
+    return $res;
+    }
+
+
+    /**
+ * Создает подготовленное выражение на основе готового SQL запроса и переданных данных
+ *
+ * @param $link mysqli Ресурс соединения
+ * @param $sql string SQL запрос с плейсхолдерами вместо значений
+ * @param array $data Данные для вставки на место плейсхолдеров
+ *
+ * @return stmt Подготовленное выражение
+ */
+function db_get_prepare_stmt_version($link, $sql, $data = []) {
+    $stmt = mysqli_prepare($link, $sql);
+
+    if ($stmt === false) {
+        $errorMsg = 'Не удалось инициализировать подготовленное выражение: ' . mysqli_error($link);
+        die($errorMsg);
+    }
+
+    if ($data) {
+        $types = '';
+        $stmt_data = [];
+
+        foreach ($data as $value) {
+            $type = 's';
+
+            if (is_int($value)) {
+                $type = 'i';
+            }
+            else if (is_double($value)) {
+                $type = 'd';
+            }
+
+            if ($type) {
+                $types .= $type;
+                $stmt_data[] = $value;
+            }
+        }
+
+        $values = array_merge([$stmt, $types], $stmt_data);
+
+        mysqli_stmt_bind_param(...$values);
+
+
+        if (mysqli_errno($link) > 0) {
+            $errorMsg = 'Не удалось связать подготовленное выражение с параметрами: ' . mysqli_error($link);
+            die($errorMsg);
+        }
+    }
+
+    return $stmt;
+}
